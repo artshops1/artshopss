aed68e661304db0088b5f339440f15c2343cc92e
artshops1 <150697492+artshops1@users.noreply.github.com>
2024-01-13 22:50:07 +0100

    Add files via upload

---
+++ Fajlovi/index.js
@@ -0,0 +1,366 @@
+// Dobijanje referenci na elemente
+var otvoriModalBtn = document.getElementById("otvoriModalBtn");
+var modalniProzor = document.getElementById("modalniProzor");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalBtn.addEventListener("click", otvoriModal);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModal() {
+    modalniProzor.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModal() {
+    modalniProzor.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzor) {
+        zatvoriModal();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModal();
+});
+
+
+
+var otvoriModalDrugiBtn = document.getElementById("otvoriModalDrugiBtn");
+var modalniProzorDrugi = document.getElementById("modalniProzorDrugi");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalDrugiBtn.addEventListener("click", otvoriModalDrugi);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalDrugi() {
+    modalniProzorDrugi.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalDrugi() {
+    modalniProzorDrugi.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorDrugi) {
+        zatvoriModalDrugi();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalDrugi();
+});
+
+
+
+var otvoriModalTreciBtn = document.getElementById("otvoriModalTreciBtn");
+var modalniProzorTreci = document.getElementById("modalniProzorTreci");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalTreciBtn.addEventListener("click", otvoriModalTreci);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalTreci() {
+    modalniProzorTreci.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalTreci() {
+    modalniProzorTreci.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorTreci) {
+        zatvoriModalTreci();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalTreci();
+});
+
+
+
+var otvoriModalCetvrtiBtn = document.getElementById("otvoriModalCetvrtiBtn");
+var modalniProzorCetvrti = document.getElementById("modalniProzorCetvrti");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalCetvrtiBtn.addEventListener("click", otvoriModalCetvrti);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalCetvrti() {
+    modalniProzorCetvrti.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalCetvrti() {
+    modalniProzorCetvrti.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorCetvrti) {
+        zatvoriModalCetvrti();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalCetvrti();
+});
+
+
+
+var otvoriModalPetiBtn = document.getElementById("otvoriModalPetiBtn");
+var modalniProzorPeti = document.getElementById("modalniProzorPeti");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalPetiBtn.addEventListener("click", otvoriModalPeti);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalPeti() {
+    modalniProzorPeti.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalPeti() {
+    modalniProzorPeti.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorPeti) {
+        zatvoriModalPeti();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalPeti();
+});
+
+
+
+var otvoriModalSestiBtn = document.getElementById("otvoriModalSestiBtn");
+var modalniProzorSesti = document.getElementById("modalniProzorSesti");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalSestiBtn.addEventListener("click", otvoriModalSesti);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalSesti() {
+    modalniProzorSesti.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalSesti() {
+    modalniProzorSesti.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorSesti) {
+        zatvoriModalSesti();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalSesti();
+});
+
+
+
+var otvoriModalSedmiBtn = document.getElementById("otvoriModalSedmiBtn");
+var modalniProzorSedmi = document.getElementById("modalniProzorSedmi");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalSedmiBtn.addEventListener("click", otvoriModalSedmi);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalSedmi() {
+    modalniProzorSedmi.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalSedmi() {
+    modalniProzorSedmi.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorSedmi) {
+        zatvoriModalSedmi();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalSedmi();
+});
+
+
+
+var otvoriModalOsmiBtn = document.getElementById("otvoriModalOsmiBtn");
+var modalniProzorOsmi = document.getElementById("modalniProzorOsmi");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalOsmiBtn.addEventListener("click", otvoriModalOsmi);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalOsmi() {
+    modalniProzorOsmi.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalOsmi() {
+    modalniProzorOsmi.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorOsmi) {
+        zatvoriModalOsmi();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalOsmi();
+});
+
+
+
+
+var otvoriModalDevetiBtn = document.getElementById("otvoriModalDevetiBtn");
+var modalniProzorDeveti = document.getElementById("modalniProzorDeveti");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalDevetiBtn.addEventListener("click", otvoriModalDeveti);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalDeveti() {
+    modalniProzorDeveti.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalDeveti() {
+    modalniProzorDeveti.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorDeveti) {
+        zatvoriModalDeveti();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalDeveti();
+});
+
+
+
+var otvoriModalDesetiBtn = document.getElementById("otvoriModalDesetiBtn");
+var modalniProzorDeseti = document.getElementById("modalniProzorDeseti");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalDesetiBtn.addEventListener("click", otvoriModalDeseti);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalDeseti() {
+    modalniProzorDeseti.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalDeseti() {
+    modalniProzorDeseti.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorDeseti) {
+        zatvoriModalDeseti();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalDeseti();
+});
+
+
+
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalDeseti();
+});
+
+
+var otvoriModalJedanestiBtn = document.getElementById("otvoriModalJedanestiBtn");
+var modalniProzorJedanseti = document.getElementById("modalniProzorJedanseti");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalJedanestiBtn.addEventListener("click", otvoriModalJedanesti);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalJedanesti() {
+    modalniProzorJedanesti.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalJedanesti() {
+    modalniProzorJedanesti.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorJedanseti) {
+        zatvoriModalJedanesti();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalJedanesti();
+});
+
+
+
+var otvoriModalDvanestiBtn = document.getElementById("otvoriModalDvanestiBtn");
+var modalniProzorDvanesti = document.getElementById("modalniProzorDvanesti");
+
+// Dodavanje event listener-a za otvaranje modalnog prozora
+otvoriModalDvanestiBtn.addEventListener("click", otvoriModalDvanesti);
+
+// Funkcija za otvaranje modalnog prozora
+function otvoriModalDvanesti() {
+    modalniProzorDvanesti.style.display = "block";
+}
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModalDvanesti() {
+    modalniProzorDvanesti.style.display = "none";
+}
+
+// Zatvori modalni prozor klikom van njega
+window.addEventListener("click", function(event) {
+    if (event.target === modalniProzorDvanesti) {
+        zatvoriModalDvanesti();
+    }
+});
+
+// Zatvori modalni prozor prilikom učitavanja stranice
+window.addEventListener("load", function() {
+    zatvoriModalDvanesti();
+});
